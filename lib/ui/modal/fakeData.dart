bd7b6149df9157858f82a0dd7261e6ca5fed98c6
Josemascherpa <joseimascherpa@gmail.com>
2024-12-05 14:03:08 -0300

    fake data

---
+++ lib/ui/modal/fakeData.dart
@@ -0,0 +1,44 @@
+import 'package:bookieapp/domain/models/Chapter.dart';
+
+List<Chapter> chaptersData = [
+  Chapter(
+    storyId: 1,
+    title: "El Inicio",
+    content: "Este es el primer capítulo de la historia.",
+    latitude: -31.6230,
+    longitude: -60.6859,
+    numberChapter: 1,
+  ),
+  Chapter(
+    storyId: 2,
+    title: "La Aventura",
+    content: "El héroe se embarca en una gran aventura.",
+    latitude: -31.6204,
+    longitude: -60.6868,
+    numberChapter: 2,
+  ),
+  Chapter(
+    storyId: 3,
+    title: "El Desafío",
+    content: "Un desafío inesperado cambia todo.",
+    latitude: -31.6210,
+    longitude: -60.6895,
+    numberChapter: 3,
+  ),
+  Chapter(
+    storyId: 4,
+    title: "El Perro",
+    content: "Se encuentra un perro en la calle.",
+    latitude: -31.6217,
+    longitude: -60.6908,
+    numberChapter: 3,
+  ),
+  Chapter(
+    storyId: 5,
+    title: "El Abismo",
+    content: "Casi cae por una montaña gigante.",
+    latitude: -31.6226,
+    longitude: -60.6911,
+    numberChapter: 3,
+  ),
+];
